62c610758417c7c1bedfb3e308560d57fb545f3c
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2024-08-24 00:21:02 +0200

    fix/429_preload_users

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,35 @@ import React, { useState, useEffect } from 'react';
 import { useFetchUsers } from './hooks/useFetchUsers';
 import UserList from './components/UserList';
 import PhantomCards from './components/PhantomCards';
+import {User} from './interfaces/interfaces'
 
 const App: React.FC = () => {
   const [page, setPage] = useState<number>(1);
   const usersPerPage = 12;
 
-  const { users, loading, error, reloadUsers, isFetching } = useFetchUsers(); // Se obtiene isFetching del hook
+  const [allUsers, setAllUsers] = useState<User[]>([]);
 
-  const displayedUsers = users.slice(0, page * usersPerPage);
+  const { users, loading, error, reloadUsers, isFetching } = useFetchUsers();
 
   useEffect(() => {
-    if (page > 1 && !loading && users.length > 0) {
-      setPage(page);
+    // When the users change, add the new users to the local state
+    if (users.length > 0) {
+      setAllUsers((prevUsers) => [...prevUsers, ...users]);
     }
-  }, [users, page, loading]);
+  }, [users]);
+
+  // Users to display on the current page
+  const displayedUsers = allUsers.slice(0, page * usersPerPage);
 
   const handleScroll = () => {
-    if (loading || isFetching) return; // Evitar que se dispare el scroll si ya estamos cargando datos
+    if (loading || isFetching) return; // Prevent the scroll event from triggering if we're already loading data
 
     if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight) {
-      if (page * usersPerPage < users.length) {
+      if (page * usersPerPage < allUsers.length) {
         setPage(prevPage => prevPage + 1);
-      } else {
-        reloadUsers(); // Solo recarga si no está en medio de un fetch
+      } else if (!isFetching) {
+        // Fetch more users if we are on the last page
+        reloadUsers();
         setPage(prevPage => prevPage + 1);
       }
     }
@@ -40,7 +46,7 @@ const App: React.FC = () => {
   return (
     <div className="flex flex-col items-center bg-gray-200 min-h-screen">
       <h1 className="text-4xl font-bold text-gray-600 my-8">Users List</h1>
-      {loading ? (
+      {loading && allUsers.length === 0 ? (
         <PhantomCards count={users.length || 6} />
       ) : (
         <div className="max-w-5xl w-full mb-20">
